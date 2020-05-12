11fd601f70a1a88d58a16a53e405caee65c714b2
NoobRider <62932698+NoobRider@users.noreply.github.com>
2020-05-12 17:20:34 +0530

    Update panel_string.py

--- tg_bot/modules/helper_funcs/panel_string.py
+++ tg_bot/modules/helper_funcs/panel_string.py
@@ -1,4 +1,4 @@
-ALLUKA_IMG = "https://telegra.ph/file/1308fbf433b0f02abaae6.jpg"
+ALLUKA_IMG = "https://telegra.ph/file/d0bd78e5bfc52c31c4271.jpg"
  
 SOURCE_STRING = """
 I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/anilchauhanxda/allukabot)
@@ -9,11 +9,11 @@ Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /source: Information about my source.
- - /settings:
+ 🦉 /start: start the bot
+ 🦉 /help: PM's you this message.
+ 🦉 /help <module name>: PM's you info about that module.
+ 🦉 /source: Information about my source.
+ 🦉 /settings:
   - in PM: will send you your settings for all supported modules.
   - in a group: will redirect you to pm, with all that chat's settings.
 {}
